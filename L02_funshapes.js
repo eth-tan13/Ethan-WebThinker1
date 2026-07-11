dc163f4a6f5f2d6d22faf0ad1b224dd290aefef5
eth-tan13 <zxethan5@gmail.com>
2026-07-11 06:56:59 +0000

    Jul 11, 2026, 6:56 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -14,7 +14,7 @@ function draw() {
 
   // Challenge 2: A face using circle() function
   circle(300,200,150)
-  circle(260,180,25)
+  circle(265,180,25)
 
   // Challenge 3: A house using rect() function
 
