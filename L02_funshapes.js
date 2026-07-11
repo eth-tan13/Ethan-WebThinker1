9047fc2c860571037f27bae5a8ebb0447222934f
eth-tan13 <zxethan5@gmail.com>
2026-07-11 07:37:07 +0000

    Jul 11, 2026, 7:37 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -30,7 +30,6 @@ function draw() {
   // Challenge 4: A rocketship using triangle() function
   ellipse(300,250,150,200);
   triangle(300,100,232,200,368,200);
-  rect(200,250,100,75)
 
   // Challenge 5: A square face using quad() function
 
