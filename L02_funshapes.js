423bf068af8ab9d3a3315f4ecb7c871c0c3e3aca
eth-tan13 <zxethan5@gmail.com>
2026-07-11 07:16:17 +0000

    Jul 11, 2026, 7:16 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
   rect(250,200,100,175);
   circle(125,150,80);
   circle(475,150,80);
-  ellipse(320,275,25,50)
+  ellipse(320,280,25,50)
 
 
   // Challenge 4: A rocketship using triangle() function
