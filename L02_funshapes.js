3d99691c009bbbbe56ff70d4ade146fad57f6e51
eth-tan13 <zxethan5@gmail.com>
2026-07-11 06:42:55 +0000

    Jul 11, 2026, 6:42 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
-  ellipse(300,)
+  ellipse(300,200,200,100);
 
   // Challenge 2: A face using circle() function
 
