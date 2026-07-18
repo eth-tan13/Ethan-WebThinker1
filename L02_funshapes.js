4155f6326be9e7d7c963d8dddb8fa4a3db3eca1b
eth-tan13 <zxethan5@gmail.com>
2026-07-18 06:21:20 +0000

    Jul 18, 2026, 6:21 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -36,7 +36,7 @@ function draw() {
   ellipse(140,200,50,25)
   ellipse(460,200,50,25)
   rect(100,187,40,25)
-  rect(500,)
+  rect(500,187,40,25)
 
   // Challenge 6: Add some colours to the drawings in the above challenges
 
