58eabdfbfef5d59dcd9ccc353104261972163459
eth-tan13 <zxethan5@gmail.com>
2026-07-25 07:40:19 +0000

    Jul 25, 2026, 7:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -65,7 +65,7 @@ function draw() {
   let totalWidth=numCircles*circleDiameter;
   let startX=(width-totalWidth)/2+circleDiameter/2;
   let startY=(height-totalWidth)/2+circleDiameter/2;
-  for(let i=0;row<5;row++) {
+  for(let row=0;row<5;row++) {
     let rowY = startY+(circleDiameter*row);
     for (let i=0;i<numCircles;i++) {
       let x=startX+i*circleDiameter;
