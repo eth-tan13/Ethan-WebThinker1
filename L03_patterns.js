c6e4a61a48e7266dc6ae6f57b215ff847923a218
eth-tan13 <zxethan5@gmail.com>
2026-07-25 07:33:42 +0000

    Jul 25, 2026, 7:33 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -65,7 +65,8 @@ function draw() {
   let totalWidth=numCircles*circleDiameter;
   let startX=(width-totalWidth)/2+circleDiameter/2;
   let startY=(height-totalWidth)/2+circleDiameter/2;
-  for(let i=0;i<numCircles;i++) {
+  for(let i=0;row<5;row++) {
+    for (let i=0;i<numCircles;i++)
     let x=startX+i*circleDiameter;
     ellipse(x,height/2,circleDiameter,circleDiameter);
   }
