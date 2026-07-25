77ef0b4d679b77b86a2c84483c3fdac0c5013cbc
eth-tan13 <zxethan5@gmail.com>
2026-07-25 06:40:19 +0000

    Jul 25, 2026, 6:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -30,9 +30,9 @@ function draw() {
     // }
 
   // Task 2: Colour Loop
-  circle(100,100,100);
-  fill(colourValue);
-  colourValue++;
+  // circle(100,100,100);
+  // fill(colourValue);
+  // colourValue++;
 
 
   // Task 3: Row of Circles
