6164c58332d88fa38b7d5f3fbeccd11468d54304
eth-tan13 <zxethan5@gmail.com>
2026-07-25 07:04:02 +0000

    Jul 25, 2026, 7:04 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -50,11 +50,11 @@ function draw() {
   // Task 4: Grid of Circles
   for (row=0;row<5;row++) {
     for (col=0;col<5;col++) {
-      // if () {
-      //   fill(0);
-      // } else {
-      //   fill(255);
-      // }
+      if ((row)) {
+        fill(0);
+      } else {
+        fill(255);
+      }
 
         rect(col*50,row*50,50,50);
     }
