0272bdb9cab29f720850b995539a7f0f8a08c6a6
eth-tan13 <zxethan5@gmail.com>
2026-08-01 06:16:51 +0000

    Aug 1, 2026, 6:16 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -7,9 +7,9 @@ function setup() {
 
 function draw() {
     fill(shapeColor);
-    ellipse(200,200,80,80)
+    ellipse(200,200,80,80);
 }
 
 function mousePressed() {
-    shapeColor = 'red' 
+    shapeColor = 'red';
 }
