eb9be574338c00ad44ac20946f6d25b391e5924c
eth-tan13 <zxethan5@gmail.com>
2026-08-01 07:00:08 +0000

    Aug 1, 2026, 7:00 AM

---
+++ L04_mouseclick.js
@@ -0,0 +1,35 @@
+let shapeColor = 'blue';
+
+// function setup() {
+//     createCanvas(400,400);
+//     background(220);
+// }
+
+// function draw() {
+//     fill(shapeColor);
+// }
+
+// function mousePressed() {
+//     let randomColor = color(random(255), random(255), random(255));
+//     fill(randomColor);
+//     noStroke();
+//     ellipse(200,200,80,80);
+// }
+
+// function mouseReleased() {
+//     shapeColor = 'white';
+//     ellipse(200,200,80,80);
+// }
+
+function setup() {
+    createCanvas(400,400);
+    background(220);
+    fill(255,0,0);
+}
+
+function mouseMove() {
+    let randomColor = color(random(255), random(255), random(255));
+    fill(randomColor);
+    noStroke();
+    ellipse(mouseX,mouseY,10,10)
+}
