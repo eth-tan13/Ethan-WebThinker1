02f638c4458b92f432c3a53172e49a251625f6cc
eth-tan13 <zxethan5@gmail.com>
2026-08-01 07:30:53 +0000

    Aug 1, 2026, 7:30 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -53,6 +53,6 @@ function mouseDragged() {
     let randomColor = color(random(255), random(255), random(255));
     fill(randomColor);
     noStroke();
-    let circleSize=
+    let circleSize=random()
     ellipse(mouseX,mouseY,20,20)
 }
