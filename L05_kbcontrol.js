1e75e13a907482b44c616bf617689c539b42181e
eth-tan13 <zxethan5@gmail.com>
2026-08-08 06:18:18 +0000

    Aug 8, 2026, 6:18 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,1 +1,18 @@
-// write your codes here
+let rectSize = 50;
+
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(220);
+    rect(width/2-rectSize/2,height/2-rectSize/2,rectSize,rectSize)
+}
+
+function keyPressed() {
+    rectSize=100;
+}
+
+function keyReleased() {
+    rectSize=50;
+}
