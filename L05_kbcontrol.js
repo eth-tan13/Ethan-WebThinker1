14affdb4d0e9d1229ad1ec8624b181c03148ba34
eth-tan13 <zxethan5@gmail.com>
2026-08-08 07:29:07 +0000

    Aug 8, 2026, 7:29 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -38,9 +38,9 @@ function draw() {
 function keyPressed() {
     console.log("key:",key);
     console.log("keyCode:",keyCode);
-    if (keyCode==48)
+    if (keyCode==67)
         circle(200,200,200);
-    if (key==32)
+    if (keyCode==32)
         square(100,100,200);
 }
 
