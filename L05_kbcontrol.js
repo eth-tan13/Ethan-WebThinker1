2b72ad578f7cf0b7bd1d4fc83a1a7a1333b33e28
eth-tan13 <zxethan5@gmail.com>
2026-08-15 07:25:01 +0000

    Aug 15, 2026, 7:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -93,7 +93,7 @@ let x=200;
 
 function draw() {
     background(220);
-    triangle(200,175,175,225,225,175);
+    triangle(x,175,175,225,225,175);
     if (keyIsDown(UP_ARROW)) {
         x-=5
     }
