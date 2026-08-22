1de45156e362cbdfcf5ff73a39abc41562e3393d
eth-tan13 <zxethan5@gmail.com>
2026-08-22 06:10:28 +0000

    Aug 22, 2026, 6:10 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -102,7 +102,7 @@ function draw() {
         y+=2;
         direction="down";
     }
-    y=constrain(y,)
+    y=constrain(y,0,height)
     if (direction=="up") {
         triangle(75,y+25,100,y-25,125,y+25);
     } else if (direction=="down") {
