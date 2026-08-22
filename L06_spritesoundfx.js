005bd00e3abf8d11aef119c8f7e632c97c54b5c6
eth-tan13 <zxethan5@gmail.com>
2026-08-22 07:30:24 +0000

    Aug 22, 2026, 7:30 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -60,7 +60,7 @@ function draw() {
     if(keyIsDown(RIGHT_ARROW)) {
         x+=speed;
     }
-    if(x===0||x===)
+    if(x===0||x===width-100) 
     x=constrain(x,0,width-100)
     y=constrain(y,0,height-100)
     image(staticImage,x,y,100,100);
