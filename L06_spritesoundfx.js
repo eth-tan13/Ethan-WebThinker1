1afc55ee8ffe843837ef0bbb9060a2b8c7739406
eth-tan13 <zxethan5@gmail.com>
2026-08-22 06:49:00 +0000

    Aug 22, 2026, 6:49 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -34,5 +34,5 @@ let soundEffect, bgMusic, staticImage;
 function preload() {
     soundEffect=loadSound('assets/pop.mp3')
     bgMusic=loadSound('assets/bossaNova.mp3')
-    staticImage=loadImage('assets')
+    staticImage=loadImage('assets/pico-a.png')
 }
