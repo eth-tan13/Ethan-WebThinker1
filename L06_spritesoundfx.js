b2b0938439f94e3c649d744e5a894b3add14e06d
eth-tan13 <zxethan5@gmail.com>
2026-08-22 07:34:08 +0000

    Aug 22, 2026, 7:34 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -63,7 +63,7 @@ function draw() {
     if(x===0||x===width-100||y===0||y===height-100) {
         background("red");
         if(!soundEffect.isPlaying()) {
-        popSound.play();
+            soundEffect.play();
         }
     }
     x=constrain(x,0,width-100)
