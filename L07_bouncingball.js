a73932f86133152bc361c3db5a83e45c4b1143b0
eth-tan13 <zxethan5@gmail.com>
2026-08-29 07:29:04 +0000

    Aug 29, 2026, 7:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -51,8 +51,8 @@
 let ballX=300;
 let ballY=200;
 let ballSize=50;
-let ballSpeedX=100;
-let ballSpeedY=100;
+let ballSpeedX=1000;
+let ballSpeedY=1000;
 
 function setup() {
     createCanvas(600,400);
