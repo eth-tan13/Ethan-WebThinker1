521f1bb5382b873e650e0f54ba724792307ce05b
eth-tan13 <zxethan5@gmail.com>
2026-08-29 07:12:33 +0000

    Aug 29, 2026, 7:12 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -62,7 +62,7 @@ function draw() {
     background(220);
     ballX+=ballSpeedX;
     ballY+=ballSpeedY;
-    if (ballX)
+    if (ballX<=0||ballX>)
     noStroke();
     fill("#fac002")
     circle(ballX,ballY,ballSize)
