5534a8fc1f0a2b7f91d4e6f988356832339b0d51
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:49:41 +0000

    Sep 12, 2026, 6:49 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -28,7 +28,7 @@ function setup() {
     createCanvas(600,400);
     background(220);
     textSize(40);
-    textAlign(SENTRE)
+    textAlign(CENTRE,CENTRE)
     text("We help you [core benefit] so you can [desired outcome].",width/2,height/2);
 }
 
