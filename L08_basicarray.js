733cdf921b7f1d3b43e704052faf2510f56878be
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:23:07 +0000

    Sep 12, 2026, 6:23 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,3 +1,9 @@
 function setup() {
-    cr
+    createCanvas(600,400);
+    background(220);
+}
+
+function draw() {
+    fill(random(255))
+    rect(width/2,height/2,100,100);
 }
--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,9 +1,1 @@
-function setup() {
-    createCanvas(600,400);
-    background(220);
-}
-
-function draw() {
-    fill(random(255))
-    rect(width/2,height/2,100,100);
-}
+// Write your code here
