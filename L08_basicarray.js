9ecf21354f95b9f2ddece1840e2d667f333049a1
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:45:38 +0000

    Sep 12, 2026, 6:45 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -28,5 +28,5 @@ function setup() {
     createCanvas(600,400);
     background(220);
     textSize(40);
-    text("gay boy",width/2,height/2);
+    text("We help you [core benefit] so you can [desired outcome].",width/2,height/2);
 }
