c754e77349205a24b7868c10c55d78a01b4b9af6
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:53:33 +0000

    Sep 12, 2026, 6:53 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -29,7 +29,7 @@ function setup() {
     background(220);
     textSize(20);
     textAlign(CENTER,CENTER)
-    text("I am ")
+    text("My name is ")
     text("We help you [core benefit] so you can [desired outcome].",width/2,height/2);
 }
 
