7c287ae5ee9463f26415ff20dbcad86833b8be85
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:43:16 +0000

    Sep 12, 2026, 6:43 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,25 +1,25 @@
-let squareX=300;
-let squareY=200;
-let squareSize=100;
-let squareSpeedX=10;
-let squareSpeedY=10;
+// let squareX=300;
+// let squareY=200;
+// let squareSize=100;
+// let squareSpeedX=100;
+// let squareSpeedY=100;
 
-function setup() {
-    createCanvas(600,400);
-    background(220);
-}
+// function setup() {
+//     createCanvas(600,400);
+//     background(220);
+// }
 
-function draw() {
-    squareX+=squareSpeedX;
-    squareY+=squareSpeedY;
-    if (squareX>=width-squareSize||squareX<=0) {
-        squareSpeedX*=-1;
-        fill(random(255),random(255),random(255));
-    }
-    if (squareY>=height-squareSize||squareY<=0) {
-        squareSpeedY*=-1;
-        fill(random(255),random(255),random(255));
-    }
-    noStroke()
-    rect(squareX,squareY,squareSize,squareSize);
-}
+// function draw() {
+//     squareX+=squareSpeedX;
+//     squareY+=squareSpeedY;
+//     if (squareX>=width-squareSize||squareX<=0) {
+//         squareSpeedX*=-1;
+//         fill(random(255),random(255),random(255));
+//     }
+//     if (squareY>=height-squareSize||squareY<=0) {
+//         squareSpeedY*=-1;
+//         fill(random(255),random(255),random(255));
+//     }
+//     noStroke()
+//     rect(squareX,squareY,squareSize,squareSize);
+// }
