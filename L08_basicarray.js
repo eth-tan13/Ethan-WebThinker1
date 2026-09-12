3f74889b5f3444844e968c82a76ce301564dd206
eth-tan13 <zxethan5@gmail.com>
2026-09-12 06:31:27 +0000

    Sep 12, 2026, 6:31 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
     squareX+=squareSpeedX
     squareY+=squareSpeedY
-    if (squareX>=width-100)
+    if (squareX>=width-100||square)
     fill(random(255),random(255),random(255))
     rect(width/2,height/2,100,100);
 }
