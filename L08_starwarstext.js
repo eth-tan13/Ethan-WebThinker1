67ff69b5393a36ff5cb603760686c5e1614ceb8d
eth-tan13 <zxethan5@gmail.com>
2026-09-19 06:24:51 +0000

    Sep 19, 2026, 6:24 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -2,7 +2,8 @@ function setup() {
     createCanvas(600,400);
     textAlign(LEFT,CENTER);
     textSize(24);
-    fill("rgb(255,255,0)")
+    fill("rgb(255,255,0)");
+    yPos
 }
 
 function draw() {
