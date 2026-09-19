616449ed73606171746f0393d30cb3c536990b61
eth-tan13 <zxethan5@gmail.com>
2026-09-19 07:33:32 +0000

    Sep 19, 2026, 7:33 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -14,5 +14,6 @@ function draw() {
     hr=hour();
     min=minute();
     sec=second();
-    timeString=nf(hr,2)+" : "+nf(min,2)+" : "+nf(sec,2)+" : ";
+    timeString=nf(hr,2)+" : "+nf(min,2)+" : "+nf(sec,2);
     text(timeString,width/2,height/2);
+}
