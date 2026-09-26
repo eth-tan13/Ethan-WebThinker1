31a5881f17f9b27deda93795e71e0e911302c5f0
eth-tan13 <zxethan5@gmail.com>
2026-09-26 07:27:52 +0000

    Sep 26, 2026, 7:27 AM

--- L10_name.js
+++ L10_name.js
@@ -3,6 +3,7 @@ let displayText="Your Name Here";
 
 function setup() {
     createCanvas(600,400);
+    
     inputText=createInput();
     inputText.position(width/2,height/2);
     inputText.input(updateText);
