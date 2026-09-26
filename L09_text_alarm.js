41005a14b0b239ade7a141ee680d4cb48fe47c9e
eth-tan13 <zxethan5@gmail.com>
2026-09-26 06:08:37 +0000

    Sep 26, 2026, 6:08 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,19 +1,21 @@
-let hr;
-let min;
-let sec;
+// let hr;
+// let min;
+// let sec;
 
-function setup() {
-    createCanvas(600,400);
-    textAlign(CENTER,CENTER);
-    textSize(40);
-    fill("rgb(255,255,0)");
-}
+// function setup() {
+//     createCanvas(600,400);
+//     textAlign(CENTER,CENTER);
+//     textSize(40);
+//     fill("rgb(255,255,0)");
+// }
 
-function draw() {
-    background(100);
-    hr=hour();
-    min=minute();
-    sec=second();
-    timeString=nf(hr,2)+" : "+nf(min,2)+" : "+nf(sec,2);
-    text(timeString,width/2,height/2);
-}
+// function draw() {
+//     background(100);
+//     hr=hour();
+//     min=minute();
+//     sec=second();
+//     timeString=nf(hr,2)+" : "+nf(min,2)+" : "+nf(sec,2);
+//     text(timeString,width/2,height/2);
+// }
+
+let
