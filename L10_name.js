a564ed248042fed7ebef6dd3d8c99d618f11dc11
eth-tan13 <zxethan5@gmail.com>
2026-09-26 07:34:27 +0000

    Sep 26, 2026, 7:34 AM

--- L10_name.js
+++ L10_name.js
@@ -7,7 +7,7 @@ function setup() {
     textSize(40)
     textAlign(CENTER,CENTER)
     inputText=createInput();
-    let inputX
+    let inputX=this.canvas.
     inputText.position(width/2,height/2);
     inputText.input(updateText);
 }
